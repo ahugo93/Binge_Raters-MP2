b49632e2c0d8686c5a9992c638e7e8d665ff4c87
Anibal Hugo <ani2bal@yahoo.com>
2022-10-26 15:15:43 -0400

    working on comment box

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,15 +1,24 @@
-import React from "react";
+import * as React from 'react';
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+import CommentBox from './CommentSection';
+
+
 
 function Popup({ selected, closePopup }) {
 	return (
+		
 		<section className="popup">
 			<div className="content">
 				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
 				<div className="plot">
-					<img src={selected.Poster} />
+					<img src={selected.Poster} alt="Poster"/>
 					<p>{selected.Plot}</p>
-					
 				</div>
+				<Stack spacing={3}>
+				<Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+	  			</Stack>
+				  <CommentBox/>
 				<button className="close" onClick={closePopup}>Close</button>
 				
 			</div>
